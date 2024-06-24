302afdaf21ee30e44ffd492e00afd994bcfeb4f0
HanXin <neil.wood@web.de>
2024-06-24 12:09:17 +0200

    started on print_report() before lunch

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ def main():
     clear_text = get_book_text(path_for_book)
     # print(clear_text)
     word_count(clear_text)
-    character_count(clear_text)
+    print(character_count(clear_text))
+    print_report()
+
+# prints out a worded report of word- and character counts
+def print_report():
+    
 
 # split() clear text and count with len()
 def word_count(text):
     words = text.split()
     print(len(words))
 
-# 
+# counts all characters and stores in dict {str : int}
 def character_count(text):
     char_count = {}
     words = text.lower().split()
@@ -23,7 +28,7 @@ def character_count(text):
                 char_count[char] += 1
             else:
                 char_count[char] = 1
-    print(char_count)
+    return char_count
 
 
 def get_book_text(path_for_book):
